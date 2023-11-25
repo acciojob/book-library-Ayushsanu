f618ff0ea0e39b286f5a826d9f57c87a62aa1dde
Ayush Srivastava <107394283+Ayushsanu@users.noreply.github.com>
2023-11-25 16:44:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 //your JS code here. If required.
+body {
+      margin: 0;
+      background-color: #f8f8f8;
+      font-family: 'Arial', sans-serif;
+    }
+
+    .book-library {
+		height:400;
+		width:800;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      background-color: #ffffff;
+      padding: 20px;
+    }
+
+    .book {
+      width: 200px;
+      background-color: #fff;
+      border: 1px solid #ddd;
+      margin: 10px;
+      padding: 10px;
+      text-align: center;
+    }
+
+    .book img {
+      max-width: 100%;
+      height: auto;
+      border-bottom: 1px solid #ddd;
+    }
+
+    .book-title {
+      font-weight: bold;
+      margin-top: 10px;
+    }
+
+    .author {
+      color: #777777;
+    }
+
+    .price-block {
+      background-color: green;
+      color: #fff;
+      padding: 5px;
+      margin-top: 5px;
+    }
